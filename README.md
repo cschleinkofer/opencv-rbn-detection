31a813e1cd52261b309f6ed4a3658b22bc324bc6
Christian Schleinkofer <christian.schleinkofer@intive.com>
2018-08-01 19:45:28 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # OpenCV RBN Detection
 
+# Install OpenCV
+
+https://coding-robin.de/2013/07/22/train-your-own-opencv-haar-classifier.html
+
+opencv_traincascade -data classifier -vec samples.vec -bg negatives.txt\
+  -numStages 20 -minHitRate 0.999 -maxFalseAlarmRate 0.5 -numPos 44\
+  -numNeg 714 -w 52 -h 40 -mode ALL -precalcValBufSize 1024\
+  -precalcIdxBufSize 1024
+
---
+++ opencv_rbndetection.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture('13-53-32.nut')
+faceCascade = cv2.CascadeClassifier('cascade_rbn.xml')
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces in the image
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.05,
+        minNeighbors=5,
+        minSize=(52, 40),
+        flags = cv2.CASCADE_SCALE_IMAGE
+    )
+
+    print "Found {0} faces!".format(len(faces))
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    cv2.imshow("Faces found", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
