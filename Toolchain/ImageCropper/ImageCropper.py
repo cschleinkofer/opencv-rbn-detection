7496431f53b23ecfffec367d86f54c312e0b7480
Christian Schleinkofer <christian.schleinkofer@intive.com>
2018-08-12 11:15:09 +0200

    ---

--- Toolchain/ImageCropper/ImageCropper.py
+++ Toolchain/ImageCropper/ImageCropper.py
@@ -1,11 +1,43 @@
 import cv2
 import argparse
+import os
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-id')
-args = parser.parse_args()
 
-print(args)
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-id')
+    parser.add_argument('-od')
+    args = parser.parse_args()
+    return args
 
+
+def get_files(path):
+    for root, dirs, files in os.walk(path):
+        for filename in files:
+            yield os.path.join(root, filename)
+
+
+def main():
+    arguments = parse_arguments()
+
+    if arguments.id is None:
+        print("No input directory provided")
+        exit(0)
+
+    if arguments.od is None:
+        print("No output directory provided")
+        exit(0)
+
+    process(arguments.id, arguments.od)
+
+
+def process(input_directory, output_directory):
+    files = get_files(input_directory)
+    for file in files:
+        print(file)
+
+
+if __name__ == "__main__":
+    main()
 
 
